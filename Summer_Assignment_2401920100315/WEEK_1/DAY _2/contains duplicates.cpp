ed834620fdd3e58071a51f1b31132e3d33083fda
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-02 07:03:25 +0530

    Update contains duplicates.cpp

--- Summer_Assignment_2401920100315/WEEK_1/DAY _2/contains duplicates.cpp
+++ Summer_Assignment_2401920100315/WEEK_1/DAY _2/contains duplicates.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int , int> um;
+        for(int x : nums){
+            um[x]++;
+        }
+        for(auto it : um){
+            if(it.second>=2){
+                 return true;
+            }    
+        }
+         return false;
+    }
+};
