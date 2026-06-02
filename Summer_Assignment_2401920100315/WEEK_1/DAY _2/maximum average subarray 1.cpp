35fb0e06190c67f41fad6b5b7669ab12d3da78ff
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-02 18:45:59 +0530

    Update maximum average subarray 1.cpp

--- Summer_Assignment_2401920100315/WEEK_1/DAY _2/maximum average subarray 1.cpp
+++ Summer_Assignment_2401920100315/WEEK_1/DAY _2/maximum average subarray 1.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+          double sum =0; 
+          for(int i=0;i<k;i++){ // creating first window of size k 
+           sum += nums[i];
+          }
+          double maxaverage = sum/k; // compute average of first window
+          for(int i = k;i<nums.size();i++){
+            sum = sum - nums[i-k] + nums[i]; // slide window 
+             maxaverage = max(sum/k,maxaverage); // compare prepare average to current average
+          }
+          return maxaverage;
+    }
+};
