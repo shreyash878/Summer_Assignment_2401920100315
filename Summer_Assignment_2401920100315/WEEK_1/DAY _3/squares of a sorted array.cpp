798e789bc678868eadeb2127c2d377124fbcc6f6
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-03 09:17:35 +0530

    Update squares of a sorted array.cpp

--- Summer_Assignment_2401920100315/WEEK_1/DAY _3/squares of a sorted array.cpp
+++ Summer_Assignment_2401920100315/WEEK_1/DAY _3/squares of a sorted array.cpp
@@ -1,1 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans; // create a new vector 
+        for(int x : nums){
+            ans.push_back(x*x); // push square value in ans
+        }
+        sort(ans.begin(),ans.end()); // sort array in ascending order
+        return ans;
 
+    }
+};
