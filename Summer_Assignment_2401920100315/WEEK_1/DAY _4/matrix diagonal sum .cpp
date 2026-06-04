a6d7db953767162f0a7bfa90cc6c6997f2223fd3
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-04 18:59:12 +0530

    Implement diagonal sum calculation in matrix

--- Summer_Assignment_2401920100315/WEEK_1/DAY _4/matrix diagonal sum .cpp
+++ Summer_Assignment_2401920100315/WEEK_1/DAY _4/matrix diagonal sum .cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int num = mat.size();
+        int sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            sum += mat[i][i];            // primary diagonal
+            sum += mat[i][num - 1 - i];    // secondary diagonal
+        }
+
+        // If num is odd, subtract the center element once
+        if (num % 2 == 1) {
+            sum -= mat[num / 2][num / 2];
+        }
+
+        return sum;
+    }
+};
 
