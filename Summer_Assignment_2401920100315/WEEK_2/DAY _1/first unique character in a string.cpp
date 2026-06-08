16843aa5f066457a3b780e8ec57d516c0ce5895a
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-08 19:56:39 +0530

    Create first unique character in a string.cpp

---
+++ Summer_Assignment_2401920100315/WEEK_2/DAY _1/first unique character in a string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> um;
+
+        for(char ch : s) {
+            um[ch]++;
+        }
+
+        for(int i = 0; i < s.size(); i++) {
+            if(um[s[i]] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
