7202edbd37d3c14616a31af062d0cd8c5f490526
shreyash878 <shreyashkashyap663@gmail.com>
2026-06-28 20:28:14 +0530

    Add Box and Box3D classes with area and volume methods

---
+++ Summer_Assignment_2401920100315/WEEK_4/OOPS/Box and Box3 main.java
@@ -0,0 +1,40 @@
+class Box {
+
+    protected double length;
+    protected double breadth;
+
+    Box(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    double area() {
+        return length * breadth;
+    }
+}
+
+class Box3D extends Box {
+
+    double height;
+
+    Box3D(double length, double breadth, double height) {
+        super(length, breadth);
+        this.height = height;
+    }
+
+    double volume() {
+        return length * breadth * height;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Box b = new Box(10, 5);
+        System.out.println("Area = " + b.area());
+
+        Box3D b3 = new Box3D(10, 5, 8);
+        System.out.println("Area = " + b3.area());
+        System.out.println("Volume = " + b3.volume());
+    }
+}
